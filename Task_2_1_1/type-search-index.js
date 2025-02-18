213d1f1eacc41ccfae2d4ec67c0b417c1a01496e
ArseniyLyskov <ArseniyLyskov@users.noreply.github.com>
2025-02-18 07:33:51 +0000

    Deploying to gh-pages from @ ArseniyLyskov/OOP@1ea662f0342e6b8327c0b8d8409b93a52e65f4d7 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.lyskov","l":"CompositeChecker"},{"p":"ru.nsu.lyskov","l":"PerformanceAnalyzer"}];updateSearchResults();
